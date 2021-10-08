080c4b6bca9cba7ff22a64af170c12b594b8821c
Bunyaev Yaroslav <yaroslav.bunyaev@tularegion.ru>
2021-10-09 01:17:56 +0300

    Add navigation between first and second screens

--- src/Navigator.tsx
+++ src/Navigator.tsx
@@ -1,32 +1,30 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import * as React from "react";
-import CardSetSelector from "./screens/CardSetSelector";
-import InitScreen from "./screens/InitScreen";
-import MythsScreens from "./screens/MythsScreens";
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import * as React from 'react';
+import CardSetSelector from './screens/CardSetSelector';
+import InitScreen from './screens/InitScreen';
+import MythsScreens from './screens/MythsScreens';
 
 const Stack = createNativeStackNavigator();
 
 const Navigator: React.FunctionComponent = () => {
   return (
-    <Stack.Navigator
-      initialRouteName={"MythsScreen"}
-    >
+    <Stack.Navigator initialRouteName={'Init'}>
       <Stack.Screen
-        name={"Init"}
+        name={'Init'}
         component={InitScreen}
         options={{headerShown: false}}
       />
       <Stack.Screen
-        name={"MythsScreen"}
+        name={'MythsScreen'}
         component={MythsScreens}
         options={{headerShown: false}}
       />
       <Stack.Screen
-        name={"CardSetSelector"}
+        name={'CardSetSelector'}
         component={CardSetSelector}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
-  )
-}
+  );
+};
 export default Navigator;
--- src/screens/InitScreen.tsx
+++ src/screens/InitScreen.tsx
@@ -1,189 +1,175 @@
-import * as React from "react";
-import {Text, View, StyleSheet, TextInput, ScrollView, Image, Button, SafeAreaView} from "react-native";
+import * as React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  Image,
+  Button,
+} from 'react-native';
 import ItemGoal from '../components/ItemGoal';
 import Slider from '@react-native-community/slider';
+import { useNavigation } from "@react-navigation/native";
 
-interface Props {
-
-}
+interface Props {}
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#172030',
-        paddingHorizontal: 16,
-        paddingVertical:12,
+  container: {
+    backgroundColor: '#172030',
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+  },
 
-    },
+  logo: {
+    paddingTop: 45,
+    paddingRight: 55,
+    alignItems: 'flex-end',
+  },
 
-    logo: {
-        paddingTop: 45,
-        paddingRight: 55,
-        alignItems: 'flex-end'
-    },
+  hello: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: 10,
+    paddingTop: 100,
+    paddingBottom: 40,
+  },
 
-    hello: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingLeft: 10,
-        paddingTop: 100,
-        paddingBottom: 40,
+  titleText: {
+    color: '#C2D4EF',
+    fontSize: 32,
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    lineHeight: 40,
+    letterSpacing: 0,
+    textAlign: 'center',
+  },
 
-    },
+  textWelcome: {
+    flexWrap: 'wrap',
+    color: '#C2D4EF',
+    fontSize: 32,
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    lineHeight: 40,
+    letterSpacing: 0,
+  },
 
-    titleText:{
-        color: '#C2D4EF',
-        fontSize: 32,
-        fontFamily: 'Roboto',
-        fontStyle: 'normal',
-        lineHeight: 40,
-        letterSpacing: 0,
-        textAlign: 'center',
-    },
+  blockQuest: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    paddingTop: 40,
+    paddingBottom: 40,
+  },
 
-    textWelcome:{
-        flexWrap: 'wrap',
-        color: '#C2D4EF',
-        fontSize: 32,
-        fontFamily: 'Roboto',
-        fontStyle: 'normal',
-        lineHeight: 40,
-        letterSpacing: 0,
-    },
+  text: {
+    color: '#C2D4EF',
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontSize: 28,
+    lineHeight: 42,
+    paddingLeft: 25,
+  },
 
-    blockQuest: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'flex-start',
-        paddingTop: 40,
-        paddingBottom: 40,
-    },
+  input: {
+    width: '90%',
+    height: 60,
+    paddingTop: 20,
+    paddingBottom: 40,
+    backgroundColor: '#7EABF0',
+    borderRadius: 20
+  },
+  slider: {
+    width: '90%',
+    height: 60,
+    paddingTop: 20,
+    paddingBottom: 40,
+    borderRadius: 20,
+  },
 
-    text:{
-        color: '#C2D4EF',
-        fontFamily: 'Roboto',
-        fontStyle: 'normal',
-        fontSize: 28,
-        lineHeight: 42,
-        paddingLeft: 25,
-    },
+  goals: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
 
-
-    input: {
-        width:'90%',
-        height: 60,
-        paddingTop: 20,
-        paddingBottom: 40,
-        backgroundColor: '#7EABF0',
-        borderRadius: 20,
-
-        underlineColorAndroid: '#172030',
-
-    },
-    slider: {
-        width:'90%',
-        height: 60,
-        paddingTop: 20,
-        paddingBottom: 40,
-        borderRadius: 20,
-    },
-
-    goals: {
-        flex: 1,
-        flexDirection:'row',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-    },
-
-    button: {
-        height: 60,
-        paddingTop: 20,
-        paddingBottom: 40,
-        backgroundColor: '#7EABF0',
-        borderRadius: 20,
-        marginVertical:20,
-    }
-
-
-})
+  button: {
+    height: 60,
+    paddingTop: 20,
+    paddingBottom: 40,
+    backgroundColor: '#7EABF0',
+    borderRadius: 20,
+    marginVertical: 20,
+  },
+});
 
 const InitScreen: React.FunctionComponent<Props> = () => {
-    const goals = [
-        {
-            goal: '8000 000'
-        },
-        {
-            goal: '500 000'
-        },
-        {
-            goal: '7 000 000'
-        },
-        {
-            goal: '100 000'
-        },
-    ]
-
+  const goals = [
+    {
+      goal: '8000 000',
+    },
+    {
+      goal: '500 000',
+    },
+    {
+      goal: '7 000 000',
+    },
+    {
+      goal: '100 000',
+    },
+  ];
+  const navigation = useNavigation()
   return (
     <ScrollView style={styles.container}>
-       <View style={styles.logo}>
-           <Image
-               style={{width: 150,
-                   height: 50,}}
-               source={{
-                   uri: 'https://reactnative.dev/img/tiny_logo.png',
-               }}
-           />
+      <View style={styles.logo}>
+        <Image
+          style={{width: 150, height: 50}}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />
       </View>
       <View style={styles.hello}>
-        <Text style={styles.titleText}>
-            Привет!
+        <Text style={styles.titleText}>Привет!</Text>
+      </View>
+
+      <View>
+        <Text style={styles.textWelcome}>
+          Классно, что ты решил разобраться с этим!
         </Text>
       </View>
 
-        <View>
-            <Text style={styles.textWelcome}>
-                Классно, что ты решил разобраться с этим!
-            </Text>
+      <View style={styles.blockQuest}>
+        <Text style={styles.text}>Как тебя зовут?</Text>
+        <TextInput style={styles.input} />
+      </View>
+
+      <View style={styles.blockQuest}>
+        <Text style={styles.text}>С чего начнем?</Text>
+        <Slider
+          style={styles.slider}
+          minimumValue={0}
+          maximumValue={1}
+          minimumTrackTintColor="#FFFFFF"
+          maximumTrackTintColor="#000000"
+        />
+      </View>
+
+      <View style={styles.blockQuest}>
+        <Text style={styles.text}>Выбери сложность</Text>
+        <View style={styles.goals}>
+          {goals.map(goal => (
+            <ItemGoal goal={goal.goal} />
+          ))}
         </View>
+      </View>
 
-        <View style={styles.blockQuest}>
-            <Text style={styles.text}>
-                Как тебя зовут?
-            </Text>
-            <TextInput
-                style={styles.input}
-            />
-        </View>
-
-
-        <View style={styles.blockQuest}>
-            <Text style={styles.text}>
-                С чего начнем?
-            </Text>
-            <Slider
-                style={styles.slider}
-                minimumValue={0}
-                maximumValue={1}
-                minimumTrackTintColor="#FFFFFF"
-                maximumTrackTintColor="#000000"
-            />
-        </View>
-
-        <View style={styles.blockQuest}>
-            <Text style={styles.text}>Выбери сложность</Text>
-            <View style={styles.goals}>
-                {goals.map(goal => <ItemGoal goal={goal.goal}  />)}
-            </View>
-        </View>
-
-
-            <Button style = {styles.button}
-                    title="Начинаем"
-            />
-
-
+      <Button style={styles.button} title="Начинаем" onPress={() => navigation.navigate('MythsScreen')} />
     </ScrollView>
-  )
-}
+  );
+};
 export default InitScreen;
 //цель - достичь инвестиционной выгоды быстрее соперника
